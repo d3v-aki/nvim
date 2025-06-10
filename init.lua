5956b55dae5e1f216f6f7c075fe413a67bb2905e
d3v-aki <dev.aki@icloud.com>
2025-06-10 20:44:05 +0800

    adding plugins lspconfig telescop ui selector

--- init.lua
+++ init.lua
@@ -1,22 +1,10 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
 
 
+--vim-opt
+require("config.vim-opt")
 
 --lazy config
 require("config.lazy")
 
---Plugins Colorscheme Keybinds
 
---require("kanagawa").setup()
---vim.cmd.colorscheme "kanagawa"
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
